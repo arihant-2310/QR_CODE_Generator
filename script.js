0f371459dcc2227e3202d8574e420588af68ada8
arihant-2310 <36256102+arihant-2310@users.noreply.github.com>
2020-03-13 08:06:21 +0000

    Update script.js

--- script.js
+++ script.js
@@ -1,18 +1,28 @@
-let qrcode = select("img");
-let qrtext = select("textarea");
-let qrbtn = select("button");
+function validate() {
+  var text = document.getElementById("text");
+  if (text.value == "") {
+    alert("Text Box cannot be empty");
+    document.getElementById("text").style.borderColor = "orange";
+    return false;
+  } else {
+    document.getElementById("text").style.borderColor = "red";
+    return true;
+  }
+}
 
-qrbtn.addEventListener("click", generateQR);
-
-function generateQR(){
+function generateQR() {
+  let qrcode = select("img");
+  let qrtext = select("textarea");
+  let qrbtn = select("button");
   let size = "1000x1000";
   let data = qrtext.value;
   let baseURL = "http://api.qrserver.com/v1/create-qr-code/";
 
   let url = `${baseURL}?data=${data}&size=${size}`;
 
-  qrcode.src= url;
+  qrcode.src = url;
 }
-function select(element){
-  return document.querySelector(element);
+
+function select(el) {
+  return document.querySelector(el);
 }
